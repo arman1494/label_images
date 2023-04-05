ae258de394904a89cfa88c7fd20b77e29978ed7c
MansouriSerenjianeh, Arman <arman.mansouri@here.com>
2023-04-05 12:50:16 +0300

    master: designed main structure

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # label_images
+
 A powerful image labeling tool designed to enhance your image processing workflow.
+
+## Titles
+
+- [Create virtualenv](#create-virtualenv)
+
+## Create virtualenv
+
+```bash
+## MacOS and Linux
+python3 -m venv venv/
+source venv/bin/activate
+
+## Windows 
+python3 -m venv venv/
+cd venv\Scripts
+activate
+```
+
+
---
+++ src/args_parser.py
@@ -0,0 +1,64 @@
+"""Module to read and validate the given arguments.
+"""
+import os
+import argparse
+
+from typing import List
+from pathlib import Path
+
+_IMAGES_SUFFIX = ['jpg', 'png', 'JPG', 'PNG']
+
+
+def gen_arguments(argv: List[str]):
+    """Reads arguments
+
+    Args:
+        argv (List[str]): The list of given argument.
+    Returns:
+        (argparse.ArgumentParser): The given arguments.
+    Raises:
+        SystemExit: Something goes wrong with input arguments.
+    """
+
+    _parser = argparse.ArgumentParser()
+
+    _parser.add_argument(
+        '-i',
+        '--imagesDir',
+        dest='images_dir',
+        type=str,
+        required=True,
+        help='The local directory path includes images to be labeled.'
+    )
+    _parser.add_argument(
+        '-o',
+        '--Output',
+        dest='output',
+        type=str,
+        required=True,
+        help='The local directory path to save the labeled images.'
+    )
+
+    return _parser.parse_args(argv)
+
+
+def does_input_dir_valid(
+        local_images_path: str):
+    """Validates the input directory path of images.
+
+    Args:
+        local_images_path (str): The given local path.
+    Rteurns:
+        None
+    Raises:
+        FileNotFoundError: When the given path does not exist.
+        ValueError: When the given directory is empty.
+    """
+    if not os.path.isdir(local_images_path):
+        raise FileNotFoundError('The given path does not refer to a directory with images content.')
+
+    for suffix in _IMAGES_SUFFIX:
+        images_names = list(Path(local_images_path).glob(f'*{suffix}'))
+        if len(images_names) == 0:
+            raise ValueError(
+                f'The given directory: "{local_images_path}" does not includes images.')
